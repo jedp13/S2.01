2ba2a8ae78b80b0f8f66dfc092a330d30cc15b09
Jed Purdy <Jed.Purdy@etu.univ-smb.fr>
2023-06-14 15:57:40 +0200

    1er essaye datacontext materiel

---
+++ MatInfo/MatInfo/WindowCM_Materiel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MatInfo
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowCM_Materiel.xaml
+    /// </summary>
+    public partial class WindowCM_Materiel : Window
+    {
+        public WindowCM_Materiel()
+        {
+            InitializeComponent();
+        }
+    }
+}
